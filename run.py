13f2e2d1c54329f6856fe5bbf8ee03b026da70f6
la_dowa57 <perry.eva05@gmail.com>
2025-06-29 22:27:33 +0200

    feat : design de la page index

--- run.py
+++ run.py
@@ -2,6 +2,3 @@ from app import create_app
 app = create_app() #Crée l'application flask 
 if __name__=='__main__':
     app.run(debug=True) #Démarre le serveur en mode débug
-
-
-#Pour tester notre application web, il faut se rendre sur : http://localhost:5000/ 
